a26b4cb147730a057f61b3ae4ef7c4e19169fa46
Andreas <andreas@soelberg.biz>
2018-05-15 23:37:32 +0200

    9911 chnaged to 9915

--- src/main/java/rest/PostScore.java
+++ src/main/java/rest/PostScore.java
@@ -34,7 +34,7 @@ public class PostScore {
     UserDTO user = new UserDTO();
         
     public PostScore() throws MalformedURLException {
-        URL url = new URL("http://ubuntu4.saluton.dk:9911/SQL_Soap?wsdl");
+        URL url = new URL("http://ubuntu4.saluton.dk:9915/SQL_Soap?wsdl");
         QName qname = new QName("http://database/", "SOAPImplService");
         Service service = Service.create(url, qname);
         soapI = service.getPort(UserDAOSOAPI.class);
