1fc1c88ea6e6a05b67bfaf79f7e55261ab37dfda
Andreas <andreas@soelberg.biz>
2018-05-17 17:47:35 +0200

    UX updated for email pass: Including prevent empty requests

--- src/main/webapp/JavaScript/forgotPass.js
+++ src/main/webapp/JavaScript/forgotPass.js
@@ -2,6 +2,9 @@
  var path = 'http://localhost:8080/mavenproject1/rest';
   
 function forgotPass(){
+    document.getElementById("forgotBtn").disabled = true;
+        document.getElementById("forgotBtn").style.opacity = 0.5;
+        document.getElementById("forgotBtn").style.cursor = "progress";
     var student={
         "student":document.getElementById("emailpsw").value
     };
@@ -18,18 +21,29 @@ function forgotPass(){
             if (send === true){
             alert('Email sent to student email');
             location.href = 'loginPage.html'
+             document.getElementById("forgotBtn").disabled = false;
+             document.getElementById("forgotBtn").style.opacity = 01;
+             document.getElementById("forgotBtn").style.cursor = "pointer";
             }
             // Login false
             else {
+                 document.getElementById("forgotBtn").disabled = false;
+                 document.getElementById("forgotBtn").style.opacity = 01;
+                 document.getElementById("forgotBtn").style.cursor = "pointer";
                      alert('Wrong student ID');
                      return false;
             }
         },
         error: function(){
-            
+             document.getElementById("forgotBtn").disabled = false;
+             document.getElementById("forgotBtn").style.opacity = 01;
+             document.getElementById("forgotBtn").style.cursor = "pointer";
             alert("Error");
         }
     });
 
     return false;
+    document.getElementById("forgotBtn").disabled = false;
+    document.getElementById("forgotBtn").style.opacity = 01;
+    document.getElementById("forgotBtn").style.cursor = "pointer";
 }
