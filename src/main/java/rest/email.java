0ec39f3fe2786bf9aa3ca1543fc5460c58e0d238
s143775 <s143775@DESKTOP-MML40HF>
2018-05-28 19:58:38 +0200

    Student + score + email response codes work.

--- src/main/java/rest/email.java
+++ src/main/java/rest/email.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Produces;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import javax.ws.rs.core.Response;
 
 @Path("email")
 public class email {
@@ -16,7 +17,7 @@ public class email {
 //send email to according to student id
     @POST
     @Produces("application/json")
-    public String email(String ID) throws java.rmi.RemoteException {
+    public Response email(String ID) throws java.rmi.RemoteException {
         JSONObject studentID = new JSONObject(ID);
 
         Brugeradmin ba = null;
@@ -31,11 +32,10 @@ public class email {
         try {
             ba.sendGlemtAdgangskodeEmail(studentID.getString("student"), "Dette er en test, husk at skifte kode");
             valid.put("Valid", true);
+            return Response.status(Response.Status.OK).build();
         } catch (IllegalStateException e) {
-            //e.printStackTrace();
             valid.put("Valid", false);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error: " + e.toString()).build();
         }
-
-        return valid.toString();
     }
 }
